74b7dc7685cbb45c867fc950016cd4e486f834a2
Nikita Vakhrameev <nikitavakhrameev@MacBook-Air-Nikita.local>
2023-02-07 21:44:04 +0300

    Corrected

--- src/index.js
+++ src/index.js
@@ -1,27 +1,46 @@
-let increment = document.querySelector
-(".increment");
-let decrement = document.querySelector(".decrement");
-let disableButtonIn = () => {
-increment.disabled = true;
-};
-let disableButtonDe = () => {
-decrement.disabled = true;
+const increment = document.querySelector(".increment");
+const decrement = document.querySelector(".decrement");
+const counter = document.querySelector(".counter");
+let value = 0;
+
+let disableIncrement = () => {
+    increment.disabled = true;
+    increment.classList.remove('button');
+    increment.classList.add('button__disabled');
 };
 
-let counter = document.querySelector(".counter");
+let disableDecrement = () => {
+    decrement.disabled = true;
+    decrement.classList.remove('button');
+    decrement.classList.add('button__disabled');
+};
+
 let increase = () => {
-decrement.disabled = false;
-counter.textContent = Number(counter.textContent) + 1;
-if (Number(counter.textContent) === 20){
-disableButtonIn ();
+    decrement.disabled = false;
+    decrement.classList.add('button');
+    decrement.classList.remove('button__disabled');
+    value += 1;
+    counter.textContent = value;
+    if (value === 20) {
+        disableIncrement ();
+    }
 }
-}
+
 let decrease = () => {
-increment.disabled = false;
-counter.textContent = Number(counter.textContent) - 1;
-if(Number(counter.textContent) === 0) {
-disableButtonDe ();
+    increment.disabled = false;
+    increment.classList.add('button');
+    increment.classList.remove('button__disabled');
+    if(value === 0) {
+        disableDecrement ();
+    } else {
+        value -= 1;
+        counter.textContent = value;
+    }
+    if (value === 0) {
+        disableDecrement ();
+    }
+    
 }
-}
+
 increment.addEventListener ("click", increase);
 decrement.addEventListener ("click", decrease);
